a4a4e105632f285210ceec46701fc123b5e6de81
Meyaal <alsuhaili_32@hotmail.com>
2023-02-20 12:52:34 +0000

    add static folder and css

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -4,25 +4,6 @@ from django.db import models
 from django.db import models
 from django.urls import reverse
 
-class MyModelName(models.Model):
-    """A typical class defining a model, derived from the Model class."""
-
-    # Fields
-    my_field_name = models.CharField(max_length=20, help_text='Enter field documentation')
-    # …
-
-    # Metadata
-    class Meta:
-        ordering = ['-my_field_name']
-
-    # Methods
-    def get_absolute_url(self):
-        """Returns the URL to access a particular instance of MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
-
-    def __str__(self):
-        """String for representing the MyModelName object (in Admin site etc.)."""
-        return self.my_field_name
 
 class Genre(models.Model):
     """Model representing a book genre."""
