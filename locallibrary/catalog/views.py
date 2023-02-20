41f56859f2988f4d48c234e73d2e6129c2ef45bc
Meyaal <alsuhaili_32@hotmail.com>
2023-02-20 13:58:37 +0000

    add book_detail.html file

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 
@@ -26,3 +27,11 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
