7d8087d119cc6491fa78ac108c659e6352ee8b9c
Meyaal <alsuhaili_32@hotmail.com>
2023-02-21 09:55:31 +0000

    add authour list file

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -34,4 +34,12 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
 
+class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
+    model = Author
+    paginate_by = 10
 
+
+class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
+    model = Author
