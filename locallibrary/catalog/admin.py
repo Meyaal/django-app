e2d3ba69d947bb3afbebb041b67c69182b11a8a6
Meyaal <alsuhaili_32@hotmail.com>
2023-03-04 07:11:51 +0000

    add class bookinstanse to admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
 
 from .models import Author, Genre, Book, BookInstance
@@ -9,3 +11,21 @@ admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
